285fd486aadcf9a86da2b046512d3d1c160df9d2
HarshForreal <hp673315@gmail.com>
2024-03-27 07:53:20 +0530

    added two buttons

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/bulb_on", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "bulb_on.png"));
+});
+
+app.get("/bulb_off", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "bulb_off.png"));
 });
 
 app.listen(PORT, () => {
